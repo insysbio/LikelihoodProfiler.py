d7638dd2e0982358286254fa6e43d169ce0af34b
Viktoria Tkachenko <tkachenko@insysbio.com>
2019-09-02 14:28:39 +0300

    up version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="likelihoodprofiler",
-      version="0.1",
+      version="0.2.1",
       description="Package for practical identifiability analysis and confidence intervals evaluation.",
       author="Evgeny Metelkin, Ivan Borisov, Victoria Tkachenko",
       author_email="metelkin@insysbio.com",

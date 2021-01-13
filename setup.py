687049c0bc3a34b566073699fbee6b3c96302033
ivborissov <iv.borissov@gmail.com>
2021-01-13 14:06:06 +0000

    Deploying to gh-pages from  @ 45fb085c320a00ff8d6549edba77f1143c7140df 🚀

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name="likelihoodprofiler",
-      version="0.3.0",
-      description="Package for practical identifiability analysis and confidence intervals evaluation.",
-      author="Evgeny Metelkin, Ivan Borisov, Victoria Tkachenko",
-      author_email="metelkin@insysbio.com",
-      packages=['likelihoodprofiler'],
-      )
